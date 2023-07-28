5a7904c736334cddbae3786ec60cda7f6a64ede4
kd077-web <96687394+kd077-web@users.noreply.github.com>
2023-07-28 14:19:26 +0545

    commit another

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,46 @@
-from threading import Thread
-class Animal(Thread) :
-    def dog(self):
-        for i in range(5):
-            print("hello buddy we all are from child")
-g=Animal()
-g.dog()
+from threading import Thread,current_thread
+class flight:
+    def __init__(self,available_seat):
+        self.available_seat=available_seat
+    def show(self,need_seat):
 
-g.start()
-g.join()  
+        if self.available_seat >= need_seat:
+            name=current_thread().name
+            print(f"the seat is available for{name}")
+            self.available_seat =self.available_seat - need_seat
+        else:
+            print("sorry the seat is not available at the moment")
+e=flight(1) 
+r=Thread(target=e.show,args=(1,),name='rahul')
+l=Thread(target=e.show,args=(1,),name='sonam')
+r.start()
+l.start()
+print("hi i am from main thread")
+#here race condition is seen so for removing race condition
 
-for i in range(5):
-    print("hello ia am from main thread")  
+from threading import *
+class flight:
     
+    def __init__(self,available_seat):
+        self.available_seat=available_seat
+        self.b=RLock()
+    def show(self,need_seat):
+
+        self.b.acquire()
+        if self.available_seat >= need_seat:
+            name=current_thread().name
+            print(f"the seat is available for{name}")
+            self.available_seat =self.available_seat - need_seat
+        else:
+            print("sorry the seat is not available at the moment")
+        self.b.release()    
+e=flight(1) 
+r=Thread(target=e.show,args=(1,),name='rahul')
+l=Thread(target=e.show,args=(1,),name='sonam')
+
+r.start()
+l.start()
+r.join()
+l.join()
+print("hi i am from main thread")
+# in rlock there is searate ac
